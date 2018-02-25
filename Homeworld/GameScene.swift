df510d5e00d00b960481d09d4c7c4eacbed652f7
Christopher Thiebaut <christopherthiebaut@gmail.com>
2018-02-24 22:03:17 -0700

    Player is a solid white block. Player can be made to fly or hover by tapping the green rectangle to apply a vertical impulse to the player.

--- Homeworld/GameScene.swift
+++ Homeworld/GameScene.swift
@@ -11,79 +11,69 @@ import GameplayKit
 
 class GameScene: SKScene {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    private var lastTouchLocation: CGPoint? = nil
+    
+    private var player: SKNode?
+    
+    private var thrustButton: SKNode?
+
     
     override func didMove(to view: SKView) {
+        physicsWorld.contactDelegate = self
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
-        
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
+        player = self.childNode(withName: "player")
+        thrustButton = self.childNode(withName: "thrust_button")
     }
     
     
     func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
+        
     }
     
     func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
+        
     }
     
     func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
+        
+    }
+    
+    private func touchWasIn(node: SKNode?) -> Bool {
+        guard let lastTouch = lastTouchLocation, let node = node else {
+            return false
+        }
+        return node.contains(lastTouch)
+    }
+    
+    private func handleTouches(touches: Set<UITouch>){
+        for touch in touches {
+            let touchLocation = touch.location(in: self)
+            lastTouchLocation = touchLocation
         }
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
-        
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
+        handleTouches(touches: touches)
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
+        handleTouches(touches: touches)
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+        handleTouches(touches: touches)
     }
     
     
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
+        if touchWasIn(node: thrustButton){
+            player?.physicsBody?.applyImpulse(CGVector.init(dx: 0, dy: 150))
+            lastTouchLocation = nil
+        }
     }
+}
+
+extension GameScene : SKPhysicsContactDelegate {
+    
 }
