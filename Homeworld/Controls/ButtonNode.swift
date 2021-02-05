e0322c1b48725e423cf03b63b33c146cc82ea1e7
Christopher Thiebaut <christopherthiebaut@gmail.com>
2021-02-05 07:16:06 -0500

    Eliminate dead code from ButtonNode

--- Homeworld/Controls/ButtonNode.swift
+++ Homeworld/Controls/ButtonNode.swift
@@ -11,34 +11,24 @@ import SpriteKit
 
 class ButtonNode: SKNode {
     
-    var buttonAction: (() -> ())?
+    var buttonAction: (() -> ())
     
     var size: CGSize {
         return self.calculateAccumulatedFrame().size
     }
-    
-    var repeatActionOnHold: Bool = false
     
     init(texture: SKTexture, size: CGSize, action: @escaping () -> () ){
         buttonAction = action
         super.init()
         let textureNode = SKSpriteNode(texture: texture, color: SKColor.white, size: size)
         self.addChild(textureNode)
-        //super.init(texture: texture, color: SKColor.white, size: size)
         isUserInteractionEnabled = true
     }
-    
-    init(texture: SKTexture, size: CGSize){
-        super.init()
-        let textureNode = SKSpriteNode(texture: texture, color: SKColor.white, size: size)
-        self.addChild(textureNode)
-        isUserInteractionEnabled = true
-    }
-    
+
     init(label: SKLabelNode, action: @escaping () -> () ){
+        self.buttonAction = action
         super.init()
         self.addChild(label)
-        self.buttonAction = action
         isUserInteractionEnabled = true
     }
     
@@ -48,14 +38,7 @@ class ButtonNode: SKNode {
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         super.touchesBegan(touches, with: event)
-        guard let action = buttonAction else {return}
-        if repeatActionOnHold {
-            run(SKAction.repeatForever(SKAction.sequence([SKAction.wait(forDuration: 0.05), SKAction.run {
-                action()
-                }])))
-        }else{
-            action()
-        }
+        buttonAction()
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
